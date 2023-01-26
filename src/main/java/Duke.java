dfaab096bd78421a0cbde684741c5e23bd563288
xin <chuabrenda@gmail.com>
2023-01-26 20:09:38 +0800

    Week 3 - Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
+
 public class Duke {
+    ;
     public static void main(String[] args) {
+        String userInput;
+        Scanner in = new Scanner(System.in);
+
+        String goodBye = "BYE";
+        String lists = "LIST";
+
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -8,30 +17,38 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo);
         System.out.println("\n1Hello! I'm Duke");
-        readLine();
-    }
-    public static void print(String command) {
 
-        String goodBye = "BYE";
-        boolean isSame = command.toUpperCase().equals(goodBye);
+        String[] list = new String[100];
 
-        if (isSame) {
-            System.out.println("Bye. Hope to see you again soon!");
+        for (int i = 0; i < 100; i ++) {
+            System.out.println("\nWhat can I do for you?\n");
+            userInput = in.nextLine();
+
+            boolean isSame = userInput.toUpperCase().equals(goodBye);
+            boolean isSame_list = userInput.toUpperCase().equals(lists);
+
+            if (isSame) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            else if (isSame_list) {
+                displayList(list);
+            }
+            else {
+                list[i] = userInput;
+                System.out.println("Added:" + userInput);
+            }
         }
-        else {
-            System.out.println(command);
-            readLine();
-        }
-
     }
-
-    public static void readLine() {
-        String userInput;
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("\nWhat can I do for you?\n");
-        userInput = in.nextLine();
-        print(userInput);
-
+    public static void displayList(String[] list) {
+        int counter = 1;
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(counter +":"+ list[i]);
+            counter++;
+            if (list[i+1] == null) {
+                break;
+            }
+        }
     }
 }
+
