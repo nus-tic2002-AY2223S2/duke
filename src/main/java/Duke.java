162cdc61881864f9fa2a384045ff4541b64fd4dc
Lim Wei Ping <iceping881129@hotmail.com>
2023-01-28 16:10:02 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -9,6 +11,10 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         String line;
+
+        // instantiate new empty ArrayList
+        ArrayList<String> lists = new ArrayList<>();
+
         Scanner in = new Scanner(System.in);
         System.out.println("Hello. I'm Duke.\nWhat can i do for you?");
 
@@ -17,8 +23,19 @@ public class Duke {
         // other than bye than print the statement
         do {
             line = in.nextLine();
-            if (!line.equals("bye")) {
-                System.out.println("_________________________________________\n" + line + "\n_________________________________________\n");
+            if (line.equals("bye")) {
+                continue;
+            }
+            else if (line.equals("list")) {
+                System.out.println("_________________________________________");
+                for (int i = 1; i < lists.size() + 1; i++) {
+                    System.out.println(i + ". " + lists.get(i - 1));
+                }
+                System.out.println("_________________________________________");
+            }
+            else {
+                System.out.println("_________________________________________\n added:" + line + "\n_________________________________________\n");
+                lists.add(line);
             }
         } while (!line.equals("bye"));
         System.out.println("_________________________________________\n Bye. Hope to see you soon again! \n_________________________________________\n");
