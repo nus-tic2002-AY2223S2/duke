c5491a4c7332ca60daf95b1f5689aae498c1857c
Lim Wei Ping <iceping881129@hotmail.com>
2023-01-28 14:31:34 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello. I'm Duke.\nWhat can i do for you?");
+
+        // use .equals to compare string
+        // if bye then exit()
+        // other than bye than print the statement
+        do {
+            line = in.nextLine();
+            if (!line.equals("bye")) {
+                System.out.println("_________________________________________\n" + line + "\n_________________________________________\n");
+            }
+        } while (!line.equals("bye"));
+        System.out.println("_________________________________________\n Bye. Hope to see you soon again! \n_________________________________________\n");
     }
 }
