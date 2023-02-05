5bba186aeb29b455dfb029b679b08f9599319070
genuis88 <genuis88@gmail.com>
2023-02-05 21:25:54 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,36 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("    ____________________________________________________________\n" +
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                "    ____________________________________________________________\n");
+
+        while (true){
+            String content;
+            System.out.print("Please key in command: ");
+            Scanner command = new Scanner(System.in);
+            content = command.nextLine();
+
+            /*---------------level-1 : ----------------------*/
+            if(content.equals("list")){
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    " + content);
+                System.out.println("    ____________________________________________________________");
+
+            }
+            else if(!content.equals("list") && !content.equals("bye")){
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    " + content);
+                System.out.println("    ____________________________________________________________");
+
+            }
+            else if(content.equals("bye")){
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________________________________");
+                break;
+            }
+        }
     }
 }
