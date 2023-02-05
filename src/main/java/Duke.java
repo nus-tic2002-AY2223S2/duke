196f54088ededde63610b06549fe6e96055eabca
genuis88 <genuis88@gmail.com>
2023-02-05 21:32:50 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -14,25 +16,30 @@ public class Duke {
                 "     What can I do for you?\n" +
                 "    ____________________________________________________________\n");
 
+        List<String> lists = new ArrayList<>();
         while (true){
             String content;
             System.out.print("Please key in command: ");
             Scanner command = new Scanner(System.in);
             content = command.nextLine();
 
-            /*---------------level-1 : ----------------------*/
-            if(content.equals("list")){
+            /*---------------level-2 : ----------------------*/
+            if(!content.equals("list") && !content.equals("bye")){
                 System.out.println("    ____________________________________________________________");
-                System.out.println("    " + content);
-                System.out.println("    ____________________________________________________________");
-
-            }
-            else if(!content.equals("list") && !content.equals("bye")){
-                System.out.println("    ____________________________________________________________");
-                System.out.println("    " + content);
+                lists.add(content);
+                System.out.println( "    " + "added: " + content);
                 System.out.println("    ____________________________________________________________");
 
             }
+            else if(content.equals("list")){
+                System.out.println("    ____________________________________________________________");
+                for(String list : lists){
+                    int listNum = lists.indexOf(list) + 1;
+                    System.out.println("    " + listNum + "." + list);
+                }
+                System.out.println("    ____________________________________________________________");
+            }
+
             else if(content.equals("bye")){
                 System.out.println("    ____________________________________________________________");
                 System.out.println("    Bye. Hope to see you again soon!");
