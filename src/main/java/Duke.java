db140e3f9b666d35f6c18b547fdedaa24fda144e
quazzelle <98038532+quaz96@users.noreply.github.com>
2023-02-07 23:42:48 +0800

    level -1
    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        
+        //Level-1
+        System.out.println("Hello! I'm Duke, the chatbot");
+        System.out.println("What can I do for you?");
+
+        Scanner in = new Scanner(System.in);
+        String command = in.nextLine();
+        if (command == "bye") {
+            System.out.println("Bye! Hope to see you again soon!");
+        } else {
+            System.out.println(command);
+        }
     }
 }
