51f93a2486ddf1843e7f1bc98391cd96b241a971
Yaoming, Kelvin Liao <e0648878@u.nus.edu>
2023-02-08 20:21:35 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,22 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?\n");
+        boolean notBye = true;
+        Scanner Obj = new Scanner(System.in);
+
+        while (notBye) {
+            String question = Obj.nextLine();
+            if (question.equals("bye")) {
+                notBye = false;
+                System.out.println("Bye. Hope to see you again soon!");
+            }
+            else {
+                System.out.println(question);
+            }
+        }
+
+
     }
 }
