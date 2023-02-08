ebf0e1b3d34c7afbc59942c894f1d970f3a83191
HappyM0ments <gabriielangs97@gmail.com>
2023-02-08 19:36:58 +0800

    Push new updates (Test)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,5 +7,6 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello again from\n" + logo);
+        System.out.println("Hello again again from\n" + logo);
     }
 }
