b03d0dcd3f970997fe79b27df0b8aeb933b023da
xin <chuabrenda@gmail.com>
2023-02-08 22:04:11 +0800

    Level 3 - Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static String[] list = new String[100];
+    static int counter = 1;
+    static String mark = "[X] ";
+    static String unmark = "[ ] ";
+
     public static void main(String[] args) {
         String userInput;
         Scanner in = new Scanner(System.in);
-        int counter = 1;
-        String toAdd;
 
         String goodBye = "BYE";
         String lists = "LIST";
-        String mark = "[X] ";
-        String unmark = "[ ] ";
+        String marked = "MARK";
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -20,33 +22,20 @@ public class Duke {
         System.out.println(logo);
         System.out.println("\nHello! I'm Duke");
 
-        String[] list = new String[100];
-
         for (int i = 0; i < 100; i++) {
             System.out.println("\nWhat can I do for you?\n");
             userInput = in.nextLine();
 
-            boolean isSame = userInput.toUpperCase().equals(goodBye);
-            boolean isSame_list = userInput.toUpperCase().equals(lists);
+            if (userInput.equalsIgnoreCase(goodBye)) { // User Input: BYE
 
-            if (isSame) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            }
-            else if (isSame_list) {
-                // displayList(list, listCounter);
-                int listCounter = 0;
-                System.out.println("Here are the tasks in your list:");
-                for (String l : list) {
-                    if (listCounter == counter - 1) {
-                    } else {
-                        System.out.println(l);
-                        listCounter++;
-                    }
-                }
+
+            } else if (userInput.equalsIgnoreCase(lists)) { // User Input: LIST
+                printList(i);
                 i--;
-            }
-            else if (userInput.toUpperCase().contains("MARK")) {
+
+            } else if (userInput.toUpperCase().contains("MARK")) { // User Input contains MARK or UNMARK
 
                 int dividerPosition = userInput.indexOf(" ");
                 String type = userInput.substring(0, dividerPosition);
@@ -54,31 +43,50 @@ public class Duke {
                 itemName = itemName.replace(" ", "");
                 int itemNum = Integer.parseInt(itemName);
 
-                if (type.toUpperCase().equals("MARK")) {
-
-                    list[itemNum - 1] = list[itemNum - 1].replace(unmark, mark);
-                    System.out.println("Nice! I've marked this task as done:");
-                    int pos = list[itemNum - 1].indexOf(".");
-                    String marked = list[itemNum - 1].substring(pos, list[itemNum - 1].length()).replace(".", " ");
-                    System.out.println(marked);
+                if (type.equalsIgnoreCase(marked)){ // User Input equals MARK
+                    i--;
+                    setToMark(itemNum);
                 }
-                else {
-
-                    list[itemNum - 1] = list[itemNum - 1].replace(mark, unmark);
-                    System.out.println("OK, I've marked this task as not done yet:");
-                    int pos = list[itemNum - 1].indexOf(".");
-                    String marked = list[itemNum - 1].substring(pos, list[itemNum - 1].length()).replace(".", " ");
-                    System.out.println(marked);
+                else { // User Input equals UNMARK
+                    i--;
+                    setToUnmark(itemNum);
                 }
-                i--;
-            }
-            else {
-                toAdd = counter + "." + unmark + userInput;
-                list[i] = toAdd;
-                System.out.println("Added:" + userInput);
-                counter++;
+
+            }  else {
+                addToList(userInput);
             }
         }
     }
-}
 
+    public static void addToList(String a) {
+        String toAdd = counter + "." + unmark + a;
+        list[counter - 1] = toAdd;
+        System.out.println("Added: " + a);
+        counter++;
+    }
+
+    public static void printList(int a) {
+        for (int i = 0; i < a; i++) {
+            System.out.println(list[i]);
+        }
+    }
+
+    public static void setToMark(int num) {
+
+        list[num - 1] = list[num - 1].replace(unmark, mark);
+        System.out.println("Nice! I've marked this task as done:");
+        int pos = list[num - 1].indexOf(".");
+        String marked = list[num - 1].substring(pos, list[num - 1].length()).replace(".", " ");
+        System.out.println(marked);
+    }
+
+    public static void setToUnmark(int num) {
+
+        list[num - 1] = list[num - 1].replace(mark, unmark);
+        System.out.println("OK, I've marked this task as not done yet:");
+        int pos = list[num - 1].indexOf(".");
+        String marked = list[num - 1].substring(pos, list[num - 1].length()).replace(".", " ");
+        System.out.println(marked);
+
+    }
+}
