196c31990d9dddaed382f580e8b5e117c4a824a8
Yaoming, Kelvin Liao <e0648878@u.nus.edu>
2023-02-08 21:00:36 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,15 +11,24 @@ public class Duke {
         System.out.println("What can I do for you?\n");
         boolean notBye = true;
         Scanner Obj = new Scanner(System.in);
-
+        int stringIndex = 0;
+        String[] list = new String[100];
         while (notBye) {
             String question = Obj.nextLine();
-            if (question.equals("bye")) {
+            if (question.equals("bye")) { //if the input is bye then end the program
                 notBye = false;
                 System.out.println("Bye. Hope to see you again soon!");
+                return; // stop the program and not continue
             }
-            else {
-                System.out.println(question);
+            else if (!question.equals("list")){ //store the string except "list"
+                list[stringIndex] = question;
+                stringIndex++;
+                System.out.println("added: " + question);
+            }
+            else if (question.equals("list")) { //loop to print out the items in list[]
+                for (int i = 0; i < stringIndex; i++) {
+                    System.out.println(i + 1 + ". " + list[i]);
+                }
             }
         }
 
