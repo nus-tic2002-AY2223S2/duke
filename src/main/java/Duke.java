1a33ed16da07cda352a1967ddd0d3892ccb134ea
HappyM0ments <gabriielangs97@gmail.com>
2023-02-08 20:19:53 +0800

    Completed: Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,38 @@
+import java.util.Scanner;  // Import Scanner class
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        //variable initialization
+        boolean factor = true;
+        Scanner scanObj = new Scanner(System.in);
+
+/*        String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("Hello again from\n" + logo);
-        System.out.println("Hello again again from\n" + logo);
+        System.out.println("Hello from\n" + logo);*/
+
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        // Get input from user
+        while(factor)
+        {
+            String input = scanObj.nextLine();
+            if(input.equals("bye"))
+            {
+                System.out.println("Bye. Hope to see you again soon!");
+                factor = false;
+            }
+            else
+            {
+                System.out.println(input);
+            }
+        }
+
+
+
+
     }
 }
