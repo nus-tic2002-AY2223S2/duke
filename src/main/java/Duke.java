98de0f059e095bb7a1cda8845f4ef1b269fed208
jeffrey4512 <jeffrey_ng1000@hotmail.com>
2023-02-09 17:36:26 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,18 +2,48 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class Duke{
+public class Duke {
+
+    public static String[] formatString(String userInput, String status) throws DukeException {
+        String formatString = "";
+        String[] stringSplit = new String[3];
+
+        switch (status) {
+            case "todo":
+                stringSplit[0] = userInput.replace("todo", "").trim();
+                if (stringSplit[0].equals("")) {
+                    throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                }
+                break;
+            case "deadline":
+                formatString = userInput.replace("deadline", "").trim();
+                stringSplit = formatString.split("/");
+                stringSplit[1] = stringSplit[1].replace("by", "").trim();
+                break;
+            case "event":
+                formatString = userInput.replace("event", "").trim();
+                stringSplit = formatString.split("/");
+                stringSplit[1] = stringSplit[1].replace("from", "").trim();
+                stringSplit[2] = stringSplit[2].replace("to", "").trim();
+                break;
+            default:
+
+
+        }
+        return stringSplit;
+    }
+
 
     public static void updateTask(String userInput, List<Task> userTask, String status) {
         int listNumber = 0;
         int currentItem = 0;
-        String formatString = "";
         String description = "";
-        String[] stringSplit = new String[100];
+        String[] stringSplit = new String[1];
+        String[] formattedString = new String[3];
         try {
-            if (status.equals("mark") || status.equals("unmark")) {
+            if (status.equals("mark") || status.equals("unmark") || status.equals("delete")) {
 
-                stringSplit = formatString.split("");
+                stringSplit = userInput.split(" ");
                 int value = Integer.parseInt(stringSplit[1]);
                 listNumber = value - 1;
             }
@@ -26,45 +56,45 @@ public class Duke{
                     userTask.get(listNumber).setStatusIconUnmarked();
                     System.out.println(userTask.get(listNumber));
                     break;
+                case "delete":
+                    System.out.println("Noted. I've removed this task:");
+                    System.out.println(userTask.get(listNumber));
+                    userTask.remove(listNumber);
+                    System.out.println("Now you have " + userTask.size() + " tasks in the list.");
+                    break;
                 case "todo":
-                    formatString = userInput.replace("todo", "").trim();
-                    if (formatString.equals("")){
-                        throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
-                    }
-                    System.out.println("Got it. I've added this task:");
+                    formattedString = formatString(userInput, status);
 
-                    userTask.add(new ToDo(formatString, "T"));
+                    userTask.add(new ToDo(formattedString[0], "T"));
                     currentItem = userTask.size() - 1;
 
+                    System.out.println("Got it. I've added this task:");
                     System.out.println(userTask.get(currentItem));
                     break;
                 case "deadline":
-                    formatString = userInput.replace("deadline", "").trim();
-                    stringSplit = formatString.split("/");
-                    description = stringSplit[0];
-                    String by = stringSplit[1].replace("by", "").trim();
+                    formattedString = formatString(userInput, status);
+                    description = formattedString[0];
+                    String by = formattedString[1];
+
                     userTask.add(new Deadlines(description, "D", by));
+                    currentItem = userTask.size() - 1;
 
                     System.out.println("Got it. I've added this task:");
-
-                    currentItem = userTask.size() - 1;
                     System.out.println(userTask.get(currentItem));
-
                     System.out.println("Now you have " + userTask.size() + " tasks in the list.");
                     break;
                 case "event":
-                    formatString = userInput.replace("event", "").trim();
-                    stringSplit = formatString.split("/");
-                    description = stringSplit[0];
-                    String from = stringSplit[1].replace("from", "").trim();
-                    String to = stringSplit[2].replace("to", "").trim();
-                    System.out.println("Got it. I've added this task:");
+                    formattedString = formatString(userInput, status);
+                    description = formattedString[0];
+                    String from = formattedString[1];
+                    String to = formattedString[2];
 
                     userTask.add(new Events(description, "E", from, to));
+
                     currentItem = userTask.size() - 1;
 
+                    System.out.println("Got it. I've added this task:");
                     System.out.println(userTask.get(currentItem));
-
                     System.out.println("Now you have " + userTask.size() + " tasks in the list.");
                     break;
                 default:
@@ -76,10 +106,7 @@ public class Duke{
         } catch (NumberFormatException e) {
             System.out.println("Please enter only numbers.");
         } catch (Exception e) {
-            //System.out.println(e);
         }
-
-
     }
 
     public static void main(String[] args) throws DukeException {
@@ -96,64 +123,55 @@ public class Duke{
 
         String userInput = "";
 
-
-
         List<Task> userTask = new ArrayList<>();
         while (!userInput.equals("bye")) {
             //Read user input.
             userInput = sc.nextLine();
-            try{
-            //Exit the chatbot.
-            if (userInput.equals("bye")) {
+            try {
+                //Exit the chatbot.
+                if (userInput.equals("bye")) {
 
-                System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println("Bye. Hope to see you again soon!");
 
-                //List out all the elements stored.
-            } else if (userInput.equals("list")) {
-
-                System.out.println("Here are the tasks in your list:");
-
-                for (int i = 0; i < userTask.size(); i++) {
-                    try {
-
-                        System.out.println((i + 1) + ". " + userTask.get(i)); 
-
-                    } catch (NumberFormatException e) {
-
-                        System.out.println("Please enter only numbers.");
-                    } catch (IndexOutOfBoundsException e) {
-
-                        System.out.println("Task number not found, please try again.");
+                    //List out all the elements stored.
+                } else if (userInput.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < userTask.size(); i++) {
+                        System.out.println((i + 1) + ". " + userTask.get(i));
                     }
+                    //Store user input and echo out.
+                } else if (userInput.startsWith("delete")) {
+                    //Deletes a task.
+                    updateTask(userInput, userTask, "delete");
+                } else if (userInput.startsWith("mark")) {
+                    //Marks a task as done.
+                    updateTask(userInput, userTask, "mark");
+
+                } else if (userInput.startsWith("unmark")) {
+                    //unmarks a task as not done.
+                    updateTask(userInput, userTask, "unmark");
+
+                } else if (userInput.startsWith("todo")) {
+                    //Create to-do task
+                    updateTask(userInput, userTask, "todo");
+
+                } else if (userInput.startsWith("deadline")) {
+                    //Create Deadline task
+                    updateTask(userInput, userTask, "deadline");
+
+                } else if (userInput.startsWith("event")) {
+                    //Create event task
+                    updateTask(userInput, userTask, "event");
+
+                } else {
+
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
 
                 }
-                //Store user input and echo out.
-            } else if (userInput.startsWith("mark")) {
-                //Marks a task as done.
-                updateTask(userInput, userTask, "mark");
-
-            } else if (userInput.startsWith("unmark")) {
-                //unmarks a task as not done.
-                updateTask(userInput, userTask, "unmark");
-
-            } else if (userInput.startsWith("todo")) {
-                updateTask(userInput, userTask, "todo");
-
-            } else if (userInput.startsWith("deadline")) {
-                updateTask(userInput, userTask, "deadline");
-
-            } else if (userInput.startsWith("event")) {
-                updateTask(userInput, userTask, "event");
-
-            }else {
-
-                throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+            } catch (Exception e) {
 
             }
-        }catch (Exception e){
-
-            }
-}
+        }
     }
 
 }
