6dbdfb6a33a58a384e82359fe5d8413f37713b96
Mamtha Tadiboina <mtadiboina@fb.com>
2023-02-10 20:08:35 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String borderLine = "____________________________________________________________";
+        String greetings = "Hello! I'm Duke \n\tWhat can I do for you";
+        System.out.printf("\t%s\n\t%s\n\t%s\n",
+                borderLine, greetings, borderLine);
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter input : ");
+
+        while(true) {
+            String userInput = input.nextLine();
+            System.out.println(userInput);
+            System.out.printf("\t%s\n\t%s\n\t%s\n",
+                    borderLine, userInput, borderLine);
+            if("bye".equalsIgnoreCase(userInput)) {
+                break;
+            }
+        }
     }
 }
