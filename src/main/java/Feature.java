c9c60cc5c7f34b90ea09e19b702496c7023532ef
Lim Wei Ping <iceping881129@hotmail.com>
2023-02-22 23:01:49 +0800

    Level 6. Create delete task. Use Enumerations for all tasks.

---
+++ src/main/java/Feature.java
@@ -0,0 +1,8 @@
+
+
+// *************************
+// level 6 ENUM
+// *************************
+public enum Feature {
+    LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE
+}
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -26,4 +26,6 @@ public class Task {
         this.isDone = false;
     }
 
+
+
 }
