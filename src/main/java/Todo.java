9b2645af90bb6c18a754113f0398cb243417af49
genuis88 <genuis88@gmail.com>
2023-03-12 21:28:24 +0800

    Add Level 7 - SAVE,OOPs

--- src/main/java/Todo.java
+++
@@ -1,8 +0,0 @@
-public class Todo extends Task{
-    public Todo(String description){
-        super(description);
-    }
-    public String toString(){
-        return "[T]" + super.toString();
-    }
-}
