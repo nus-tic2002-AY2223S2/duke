9c75e3c35141e459320649cb80bd2e0267205dd1
Lily <zlily0120@gmail.com>
2023-03-26 19:19:33 +0800

    Level-1 Greet,Echo,Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " \t .ᕱ⠀⠀⠀ᕱ⠀ ⠀➶ ⠀➴        \n"
+                + "\t \u200E(๑◕ܫ◕๑) ➶⠀⠀⠀ ⠀➴ \n"
+                + "\t \u200E૮⠀⠀⑅ ⠀づ ⠀⠀⠀⠀⠀⠀⠀➵ Let's Chat with me ♡\n";
+
+        String breakLine = "\t♡____________________________________________________________♡\n";
+        String text;
+        Scanner input = new Scanner(System.in);
+        System.out.println("\t Hello from Lily \u200E|•'-'•) ✧\n" + logo);
+        System.out.println(breakLine+"\t ᑋᵉᑊᑊᵒ ᵕ̈ ᑋᵉᑊᑊᵒ I'm Dululu\n" + "\t What can I do for you? (°◡°♡).:｡\n" + breakLine);
+        text = input.nextLine();
+        //System.out.println(breakLine + text + breakLine);
+        while(!text.equalsIgnoreCase("bye")) {
+            System.out.printf("%s\t %s\n%s", breakLine, text, breakLine);
+            text = input.nextLine();
+        }
+        System.out.println(breakLine + "\t Bye( '༥' ). Hope to see you again soon!\n"+breakLine);
     }
 }
