adc6c85d7e6035addb53d7eb4c79766d9babb35b
Teo Jun Hao <>
2023-03-30 18:09:05 +0800

    no message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
+
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,5 +10,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Greetings from\n" + logo);
+        System.out.println("So... how can I assist you?");
+
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        System.out.println(line);
     }
 }
+
