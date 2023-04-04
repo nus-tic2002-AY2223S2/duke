457972349c8d442be8bdfb8dc1cdda003a4e2d73
quazzelle <98038532+quaz96@users.noreply.github.com>
2023-04-05 00:12:35 +0800

    Level-8.2
    Until JavaDoc

---
+++ src/main/java/task/Event.java
@@ -0,0 +1,34 @@
+package task;
+
+import task.Task;
+
+/**
+ * event task
+ * -> returns the specific event message
+ */
+public class Event extends Task{
+    protected String from;
+    protected String to;
+
+    /**
+     *
+     * @param description -> event's description
+     * @param from -> start of events'date/time
+     * @param to -> end of events'date/time
+     */
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+    }
+
+    @Override
+    public String toSave() {
+        return "E " + super.toSave() + " | " + from + "-" + to;
+    }
+}
---
+++ src/main/java/task/TaskList.java
@@ -0,0 +1,58 @@
+package task;
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import task.Deadline;
+import task.ToDo;
+import task.Event;
+import task.TaskList;
+
+/**
+ * tasklist
+ * -> helps to get all the tasks, store the tasks and run other commands
+ */
+public class TaskList {
+    public static ArrayList<Task> taskslist;
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.taskslist = tasks;
+    }
+
+    public TaskList() {
+        taskslist = new ArrayList<Task>();
+    }
+
+    public ArrayList<Task> getTasks(){
+        return taskslist;
+    }
+
+    public void getSize() {
+        System.out.println(taskslist.size());
+    }
+
+
+    public void addDeadline(String deadLineTask, String lastDay) {
+        taskslist.add(new Deadline(deadLineTask, lastDay));
+        System.out.println(deadLineTask);
+    }
+
+    public void addTodo(String todoTask) {
+        taskslist.add(new ToDo(todoTask));
+        System.out.println(todoTask);
+    }
+
+    public void addEvent(String eventTask, String fromEvent, String toEvent) {
+        taskslist.add(new Event(eventTask, fromEvent, toEvent));
+        System.out.println(eventTask);
+    }
+
+    public void delete(int i) {
+        System.out.println(taskslist.get(i));
+        taskslist.remove(i);
+    }
+
+
+
+}
