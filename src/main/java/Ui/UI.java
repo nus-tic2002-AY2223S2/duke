8e0eec9755766b53e5c37239e773d184b1e67c33
genuis88 <genuis88@gmail.com>
2023-04-07 01:11:28 +0800

    Level 10 updated.

--- src/main/java/Ui/UI.java
+++ src/main/java/Ui/UI.java
@@ -1,7 +1,10 @@
 package ui;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+
 import exception.DukeException;
 import task.Task;
 import taskslist.TasksList;
@@ -20,6 +23,41 @@ public class Ui {
         Ui.showLine();
         System.out.println(space + "Hello! I'm Duke\n" + space +"What can I do for you?");
         Ui.showLine();
+    }
+
+    /**
+     * Show to the user about Duke menu
+     */
+    public static void showUserMenu(){
+        Set<String> hashSet = new HashSet<>();
+        String space = " ";
+        System.out.println("Please key in command followed as below:");
+        String LIST_ALL_TASKS = "-List all existing tasks, enter list\n";
+        String TODO_TASK = "-Add a todo task, enter todo XXX\n";
+        String DEADLINE_TASK = "-Add a deadline task, enter deadline XXX /by \"yyyy-MM-dd HH:mm\"\n";
+        String EVENT_TASK = "-Add a event task, enter event XXX /from \"yyyy-MM-dd HH:mm\" /to \"yyyy-MM-dd HH:mm\"\n";
+        String MARK_TASK = "-Mark task, enter mark [task index]\n";
+        String UNMARK_TASK = "-Unmark task, enter unmark [task index]\n";
+        String DELETE_TASK = "-Delete task, enter delete [task index]\n";
+        String FIND_TASK = "-Find task, enter find XXX\n";
+        String SCHEDULE_TASK = "-View the schedule for a specific date, enter schedule \"yyyy-MM-dd HH:mm\"\n";
+        String SAVE_TASK = "-Save task, enter save\n";
+        String EXIT = "-To exit Duke, enter bye\n";
+        hashSet.add(LIST_ALL_TASKS);
+        hashSet.add(TODO_TASK);
+        hashSet.add(DEADLINE_TASK);
+        hashSet.add(EVENT_TASK);
+        hashSet.add(MARK_TASK);
+        hashSet.add(UNMARK_TASK);
+        hashSet.add(DELETE_TASK);
+        hashSet.add(FIND_TASK);
+        hashSet.add(SCHEDULE_TASK);
+        hashSet.add(SAVE_TASK);
+        hashSet.add(EXIT);
+        for(String s: hashSet){
+            System.out.print(space + s);
+        }
+        System.out.println();
     }
 
     /**
