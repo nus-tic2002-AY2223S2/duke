0fe7d0d9c0e255b55f107596fb352a3cafa04dda
yuhanyuhan <yuhanlian@gmail.com>
2023-04-08 23:07:13 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,21 +5,47 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> task = new ArrayList<String>();
-        int index = task.toArray().length + 1;
+        // create an array of task
+        ArrayList<Task> list_of_task = new ArrayList<Task>();
 
         while (scanner.hasNext()) {
             String input = scanner.nextLine();
-            if (!input.equals("list") && !input.equals("bye")) {
-                task.add(input);
-                index++;
-                System.out.println("added:" + input);
+            Task new_task = new Task(input);
+
+            // add to-do list
+            if (!input.equals("list") && !input.equals("bye") && !input.contains("mark") && !input.contains("unmark")) {
+                list_of_task.add(new_task);
+                System.out.println("added:" + new_task.description);
+            }
+
+            if (input.matches("mark(.*)")) {
+                String num_input = input.replaceAll("\\D+","");
+                int i = Integer.parseInt(num_input);
+
+                if (i > list_of_task.size()) continue;
+                list_of_task.get(i - 1).markDone();
+
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println(list_of_task.get(i - 1).getDescription());
+            }
+
+            if (input.matches("unmark(.*)")) {
+                String num_input = input.replaceAll("\\D+","");
+                int i = Integer.parseInt(num_input);
+
+                if (i > list_of_task.size()) continue;
+                list_of_task.get(i - 1).markUnDone();
+
+                System.out.println("OK, I've marked this task as not done yet: ");
+                System.out.println(list_of_task.get(i - 1).getDescription());
             }
 
             if (input.equals("list")) {
-                for (int i = 0; i < task.size(); i++)
+                if (list_of_task.isEmpty()) continue;
+                System.out.println("Here are the tasks in your list: ");
+                for (int index = 0; index < list_of_task.size(); index++)
                 {
-                    System.out.println(i+1 + ". " + task.get(i));
+                    System.out.println(index+1 + ". " + list_of_task.get(index).getDescription());
                 }
             }
 
@@ -27,6 +53,7 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!\n");
                 break;
             }
+
         }
         scanner.close();
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    ArrayList<String> task = new ArrayList();
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+    public void markDone() {
+        if (isDone) return;
+        this.isDone = true;
+    }
+
+    public void markUnDone() {
+        if (!isDone) return;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return "["+ getStatusIcon() + "]" + this.description;
+    }
+
+}
