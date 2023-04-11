482c3dae412c073999fa70ab77800129153bad07
quazzelle <98038532+quaz96@users.noreply.github.com>
2023-04-11 21:28:28 +0800

    Level-9 Update

--- src/main/java/task/Event.java
+++ src/main/java/task/Event.java
@@ -21,6 +21,22 @@ public class Event extends Task{
         this.from = from;
         this.to = to;
     }
+    
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
 
     @Override
     public String toString() {
