df6ab141770cffac91d41c0c41bd1741b683a9e3
quazzelle <98038532+quaz96@users.noreply.github.com>
2023-04-11 19:08:42 +0800

    Edit printMsg

--- src/main/java/command/MarkCommand.java
+++ src/main/java/command/MarkCommand.java
@@ -21,6 +21,6 @@ public class MarkCommand extends Commands{
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         tasks.taskslist.get(posNo).mark();
-        ui.printMsg("Ok, I've marked this task as not done yet:" + tasks.taskslist.get(posNo).toString());
+        ui.printMsg("Ok, I've marked this task as done: " + tasks.taskslist.get(posNo).toString());
     }
 }
