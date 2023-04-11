bf3af3a83b96c714ec1b4dd97e26fa6fdfd3abf9
quazzelle <98038532+quaz96@users.noreply.github.com>
2023-04-11 21:28:04 +0800

    Level-9 Update

--- src/main/java/task/Deadline.java
+++ src/main/java/task/Deadline.java
@@ -21,6 +21,14 @@ public class Deadline extends Task{
         super(description);
         this.by = by;
     }
+    
+    public String getBy() {
+        return by;
+    }
+
+    public void setBy(String by) {
+        this.by = by;
+    }
 
     @Override
     public String toString() {
