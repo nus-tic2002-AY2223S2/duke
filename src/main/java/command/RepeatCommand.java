7dae04ed6620e98c4449c368fcf71dd8f64e6611
quazzelle <98038532+quaz96@users.noreply.github.com>
2023-04-13 00:08:31 +0800

    Level 10 - update recurring task

--- src/main/java/command/RepeatCommand.java
+++ src/main/java/command/RepeatCommand.java
@@ -4,7 +4,7 @@ import task.*;
 import ui.Ui;
 
 /**
- * Update Command
+ * Repeat Command
  * -> To manage repeated task
  */
 public class RepeatCommand extends Commands{
@@ -16,8 +16,28 @@ public class RepeatCommand extends Commands{
 
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
-        Task task = tasks.taskslist.get(posNo);
-        Ui.printMsg("Task to repeat is " + task.toString());
-        tasks.taskslist.add(task);
+        if (posNo >=0 && posNo<tasks.taskslist.size()) {
+            Task task = tasks.taskslist.get(posNo);
+            Ui.printMsg("Selected task for recurring is " + task.toString());
+
+            if (task.toString().contains("[T]")) { // to prevent repeated marking
+                String repeatTask = task.getDescription();
+                tasks.addTodo(repeatTask);
+            } else if (task.toString().contains("[D]")) {
+                Deadline d = (Deadline) task;
+                String repeatTask = d.getDescription();
+                String repeatDate = d.getBy();
+                tasks.addDeadline(repeatTask, repeatDate);
+            } else if (task.toString().contains("[E]")) {
+                Event e = (Event) task;
+                String repeatTask = e.getDescription();
+                String repeatFrom = e.getFrom();
+                String repeatTo = e.getTo();
+                tasks.addEvent(repeatTask, repeatFrom,repeatTo);
+            }
+        } else {
+            Ui.printMsg("Invalid task number " + posNo);
+        }
+        ui.showLine();
     }
 }
