bcd4921b64ff5e9db2bd985378152d0edea0aa10
quazzelle <98038532+quaz96@users.noreply.github.com>
2023-04-16 00:11:28 +0800

    update

--- src/main/java/command/EventCommand.java
+++ src/main/java/command/EventCommand.java
@@ -27,11 +27,15 @@ public class EventCommand extends Commands{
 
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
-        Ui.addedTaskMsg();
-        tasks.addEvent(description, from, to);
-        Ui.printMsg(tasks.taskslist.get(tasks.taskslist.size()-1).toString());
-        Ui.printMsg("Number of tasks in the list = " + tasks.taskslist.size());
-        ui.showLine();
+         if (from.isEmpty() || to.isEmpty()) {
+            ui.showLine();
+        } else {
+            Ui.addedTaskMsg();
+            tasks.addEvent(description, from, to);
+            Ui.printMsg(tasks.taskslist.get(tasks.taskslist.size()-1).toString());
+            Ui.printMsg("Number of tasks in the list = " + tasks.taskslist.size());
+            ui.showLine();
+        }
     }
 
 }
