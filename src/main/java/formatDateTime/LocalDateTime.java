60f44c1ba701272aac28d52e658027a91f94a3a7
quazzelle <98038532+quaz96@users.noreply.github.com>
2023-04-16 00:12:51 +0800

    update

--- src/main/java/formatDateTime/LocalDateTime.java
+++ src/main/java/formatDateTime/LocalDateTime.java
@@ -23,32 +23,41 @@ public class LocalDateTime {
      * @return
      */
     public static String formatDate(String dateTime) {
-        String updatedDateTime = dateTime;
+        String updatedDateTime = "";
         try {
-            DateTimeFormatter formatting = DateTimeFormatter.ofPattern("d/MM/yyyy HHmm");
-            java.time.LocalDateTime date = java.time.LocalDateTime.parse(dateTime, formatting);
+            if (dateTime.contains("/")) {
+                DateTimeFormatter formatting = DateTimeFormatter.ofPattern("d/MM/yyyy HHmm");
+                java.time.LocalDateTime date = java.time.LocalDateTime.parse(dateTime, formatting);
 
-            //get the date
-            String[] numDate = dateTime.split("/");
-            switch (numDate[0]) {
-                case "1":
-                case "01":
-                    DateTimeFormatter outPutOne = DateTimeFormatter.ofPattern("d'st' 'of' MMMM yyyy, ha", Locale.ENGLISH);
-                    updatedDateTime = date.format(outPutOne);
-                    break;
-                case "2":
-                case "02":
-                    DateTimeFormatter outPutTwo = DateTimeFormatter.ofPattern("d'nd' 'of' MMMM yyyy, ha", Locale.ENGLISH);
-                    updatedDateTime = date.format(outPutTwo);
-                    break;
-                case "3":
-                case "03":
-                    DateTimeFormatter outPutThree = DateTimeFormatter.ofPattern("d'rd' 'of' MMMM yyyy, ha", Locale.ENGLISH);
-                    updatedDateTime = date.format(outPutThree);
-                    break;
-                default:
-                    DateTimeFormatter outPut = DateTimeFormatter.ofPattern("d'th' 'of' MMMM yyyy, ha", Locale.ENGLISH);
-                    updatedDateTime = date.format(outPut);
+                //get the date
+                String[] numDate = dateTime.split("/");
+
+                    switch (numDate[0]) {
+                        case "1":
+                        case "01":
+                        case "21":
+                        case "31":
+                            DateTimeFormatter outPutOne = DateTimeFormatter.ofPattern("d'st' 'of' MMMM yyyy, ha", Locale.ENGLISH);
+                            updatedDateTime = date.format(outPutOne);
+                            break;
+                        case "2":
+                        case "02":
+                        case "22":
+                            DateTimeFormatter outPutTwo = DateTimeFormatter.ofPattern("d'nd' 'of' MMMM yyyy, ha", Locale.ENGLISH);
+                            updatedDateTime = date.format(outPutTwo);
+                            break;
+                        case "3":
+                        case "03":
+                        case "23":
+                            DateTimeFormatter outPutThree = DateTimeFormatter.ofPattern("d'rd' 'of' MMMM yyyy, ha", Locale.ENGLISH);
+                            updatedDateTime = date.format(outPutThree);
+                            break;
+                        default:
+                            DateTimeFormatter outPut = DateTimeFormatter.ofPattern("d'th' 'of' MMMM yyyy, ha", Locale.ENGLISH);
+                            updatedDateTime = date.format(outPut);
+                    }
+            } else {
+                updatedDateTime = dateTime;
             }
         } catch (DateTimeParseException e) {
             Ui.printMsg("Invalid date and time format");
