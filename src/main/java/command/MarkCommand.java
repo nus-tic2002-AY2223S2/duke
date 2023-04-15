36c7fc316ea7e4967467ad709d5115f0b0fec39a
quazzelle <98038532+quaz96@users.noreply.github.com>
2023-04-16 00:12:03 +0800

    update

--- src/main/java/command/MarkCommand.java
+++ src/main/java/command/MarkCommand.java
@@ -20,8 +20,12 @@ public class MarkCommand extends Commands{
 
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
-        tasks.taskslist.get(posNo).mark();
-        ui.printMsg("Ok, I've marked this task as done: " + tasks.taskslist.get(posNo).toString());
-        ui.showLine();
+        try {
+            tasks.taskslist.get(posNo).mark();
+            ui.printMsg("Ok, I've marked this task as done: " + tasks.taskslist.get(posNo).toString());
+            ui.showLine();
+        } catch (IndexOutOfBoundsException e) {
+            Ui.printMsg("The number is greater than the number of tasks in the list :(");
+        }
     }
 }
