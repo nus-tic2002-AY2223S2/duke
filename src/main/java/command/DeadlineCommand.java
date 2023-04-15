ea1c52ed72f789a63d96aefbd82016f72a8e7702
quazzelle <98038532+quaz96@users.noreply.github.com>
2023-04-16 00:11:02 +0800

    update

--- src/main/java/command/DeadlineCommand.java
+++ src/main/java/command/DeadlineCommand.java
@@ -23,11 +23,15 @@ public class DeadlineCommand extends Commands{
 
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
-        Ui.addedTaskMsg();
-        tasks.addDeadline(description, by);
-        Ui.printMsg(tasks.taskslist.get(tasks.taskslist.size()-1).toString());
-        Ui.printMsg("Number of tasks in the list = " + tasks.taskslist.size());
-        ui.showLine();
+        if (by.isEmpty()) {
+            ui.showLine();
+        } else {
+            Ui.addedTaskMsg();
+            tasks.addDeadline(description, by);
+            Ui.printMsg(tasks.taskslist.get(tasks.taskslist.size() - 1).toString());
+            Ui.printMsg("Number of tasks in the list = " + tasks.taskslist.size());
+            ui.showLine();
+        }
     }
 
 }
